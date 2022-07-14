273fc9789e99eaace1b3141561d95a1bf8615ea0
dcen7 <mdcengiz@gmail.com>
2022-07-14 09:22:43 -0500

    Create a data model, ReviewItem ,RestaurantPhotoItem,Core Data Manager

---
+++ LetsEat/Misc/Core Data/CoreDataManager.swift
@@ -0,0 +1,92 @@
+//
+//  CoreDataManager.swift
+//  LetsEat
+//
+//  Created by Mehmet Deniz Cengiz on 7/13/22.
+//
+
+import Foundation
+import CoreData
+
+struct CoreDataManager {
+    let container: NSPersistentContainer
+    init() {
+        container = NSPersistentContainer(name: "LetsEatMode")
+        container.loadPersistentStores { (storeDesc, error) in
+            error.map {
+                print($0)
+            }
+        }
+    }
+    
+    func addReview(_ reviewItem: ReviewItem) {
+        let review = Review(context: container.viewContext)
+        review.date = Date()
+        if let reviewItemRating = reviewItem.rating {
+            review.rating = reviewItemRating
+        }
+        review.title = reviewItem.title
+        review.name = reviewItem.name
+        review.customerReview = reviewItem.customerReview
+        if let reviewItemRestID = reviewItem.restaurantID {
+            review.restaurantID = reviewItemRestID
+        }
+        review.uuid = reviewItem.uuid
+        save()
+    }
+    
+    func addPhoto(_ restPhotoItem: RestaurantPhotoItem) {
+        let restPhoto = RestaurantPhoto(context: container.viewContext)
+        restPhoto.date = Date()
+        restPhoto.photo = restPhotoItem.photoData
+        if let restPhotoID = restPhotoItem.restaurantID {
+            restPhoto.restaurantID = restPhotoID
+        }
+        restPhoto.uuid = restPhotoItem.uuid
+        save()
+    }
+    
+    func fecthReviews(by identifier: Int) -> [ReviewItem] {
+        let moc = container.viewContext
+        let request = Review.fetchRequest()
+        let predicate = NSPredicate(format: "restaurantID = %i", identifier)
+        var reviewItems: [ReviewItem] = []
+        request.sortDescriptors = [NSSortDescriptor(key: "date", ascending: false)]
+        request.predicate = predicate
+        do {
+            for review in try moc.fetch(request) {
+                reviewItems.append(ReviewItem(review: review))
+            }
+            return reviewItems
+        } catch {
+            fatalError("Failed to fetch reviews: \(error)")
+        }
+    }
+    
+    func fetchRestPhotos(by identifier: Int) -> [RestaurantPhotoItem] {
+        let moc = container.viewContext
+        let request = RestaurantPhoto.fetchRequest()
+        let predicate = NSPredicate(format: "restaurantID = %i", identifier)
+        var restPhotoItems: [RestaurantPhotoItem] = []
+        request.sortDescriptors = [NSSortDescriptor(key: "date", ascending: false)]
+        request.predicate = predicate
+        do {
+            for restPhoto in try moc.fetch(request) {
+                restPhotoItems.append(RestaurantPhotoItem(restaurantPhoto:restPhoto))
+            }
+            return restPhotoItems
+        } catch {
+            fatalError("Failed to fetch restaurant photos: \(error)")
+        }
+    }
+    
+    private func save() {
+        do {
+            if container.viewContext.hasChanges {
+                try container.viewContext.save()
+            }
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+}
---
+++ LetsEat/PhotoFilter/Modal/RestaurantPhotoItem.swift
@@ -0,0 +1,34 @@
+//
+//  RestaurantPhotoItem.swift
+//  LetsEat
+//
+//  Created by Mehmet Deniz Cengiz on 7/13/22.
+//
+
+import UIKit
+
+struct RestaurantPhotoItem {
+    var date: Date?
+    var photo: UIImage?
+    var photoData: Data {
+        guard let photo = photo, let photoData = photo.pngData() else {
+            return Data()
+        }
+        return photoData
+    }
+    var restaurantID: Int64?
+    var uuid = UUID()
+}
+
+extension RestaurantPhotoItem {
+    init(restaurantPhoto: RestaurantPhoto) {
+        self.date = restaurantPhoto.date
+        if let restPhoto = restaurantPhoto.photo {
+            self.photo = UIImage(data: restPhoto, scale: 1.0)
+        }
+        self.restaurantID = restaurantPhoto.restaurantID
+        if let restPhotoUUID = restaurantPhoto.uuid {
+            self.uuid = restPhotoUUID
+        }
+    }
+}
